fa661b8fb06158d811578845a0175ee207a30cbc
HayesHiller <hayeshiller@hotmail.com>
2020-11-12 20:27:58 -0700

    adding in first couple questions

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+// External Packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input', 
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if(answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {}
 ];
 
 // function to write README file
