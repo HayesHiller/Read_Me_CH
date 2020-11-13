65a0670f5f6813df3110b55c22cb2bcf9f2a13f5
HayesHiller <hayeshiller@hotmail.com>
2020-11-12 21:01:31 -0700

    finishing last questions and finishing the rest of the index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const util = require('util');
 // Internal modules
 const api = require('./utils/api.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
 
 // array of questions for user
 const questions = [
@@ -33,17 +34,96 @@ const questions = [
             return true;
         }
     },
-    {}
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description', 
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A validate project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'contributing'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Succes! Your README.md file has been generated");
+    });
 }
+
+const writeFileAsync = util.promisify(writeToFile);
+
+
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
 
-}
+        // Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
 
+        // GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+
+        // Pass Inquirer userResponses and GitHub userInfo to generate Markdown
+        console.log("Generating your README next...");
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+
+        // Markdown to file
+        await writeFileAsync('ExampleREAD.md', markdown);
+    
+    } catch (error) {
+        console.log(error);
+    } 
+};
 // function call to initialize program
 init();
